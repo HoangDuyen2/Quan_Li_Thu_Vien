ed353e338b0b36f06126510526722a73a446c52c
HoangDuyen2 <148479791+HoangDuyen2@users.noreply.github.com>
2024-04-07 00:12:39 +0700

    Update CreateTrigger.sql

--- DB_QLTV/CreateTrigger.sql
+++ DB_QLTV/CreateTrigger.sql
@@ -1,1 +1,21 @@
-
+--Kiểm tra số điện thoại trùng của Nhà cung cấp
+CREATE TRIGGER CC_TrungSDT
+ON CungCap
+AFTER INSERT, UPDATE
+AS
+BEGIN
+ -- Kiểm tra số điện thoại vừa thêm có bị trùng lặp
+	IF EXISTS (
+			 SELECT *
+			 FROM inserted i
+			 WHERE EXISTS (
+				 SELECT *
+				 FROM CungCap cc
+				 WHERE cc.SoDienThoai = i.SoDienThoai AND cc.MaNhaCC <> i.MaNhaCC
+			 ))
+	 BEGIN
+		 -- Nếu trùng thì rollback
+		 --PRINT N'Số điện thoại đã tồn tại'
+		 ROLLBACK;
+	 END
+END
