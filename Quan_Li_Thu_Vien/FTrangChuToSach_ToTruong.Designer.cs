982afc7515333adf11068d1735e359b4f1a477ff
Duyen <thihoangduyendo@gmail.com>
2024-04-07 16:21:50 +0700

    Form Trang chủ của tổ sách, view Tổ trưởng

---
+++ Quan_Li_Thu_Vien/FTrangChuToSach_ToTruong.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Quan_Li_Thu_Vien
+{
+    partial class FTrangChuToSach_ToTruong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelNen = new System.Windows.Forms.Panel();
+            this.picBoxNen = new System.Windows.Forms.PictureBox();
+            this.panelMucLuc = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnXemDSSach = new System.Windows.Forms.Button();
+            this.panelNen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxNen)).BeginInit();
+            this.panelMucLuc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelNen
+            // 
+            this.panelNen.Controls.Add(this.picBoxNen);
+            this.panelNen.Controls.Add(this.panelMucLuc);
+            this.panelNen.Location = new System.Drawing.Point(0, 0);
+            this.panelNen.Name = "panelNen";
+            this.panelNen.Size = new System.Drawing.Size(1052, 600);
+            this.panelNen.TabIndex = 0;
+            // 
+            // picBoxNen
+            // 
+            this.picBoxNen.BackgroundImage = global::Quan_Li_Thu_Vien.Properties.Resources.biaAnh;
+            this.picBoxNen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBoxNen.Location = new System.Drawing.Point(212, 1);
+            this.picBoxNen.Name = "picBoxNen";
+            this.picBoxNen.Size = new System.Drawing.Size(840, 599);
+            this.picBoxNen.TabIndex = 1;
+            this.picBoxNen.TabStop = false;
+            // 
+            // panelMucLuc
+            // 
+            this.panelMucLuc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelMucLuc.Controls.Add(this.button4);
+            this.panelMucLuc.Controls.Add(this.button3);
+            this.panelMucLuc.Controls.Add(this.button2);
+            this.panelMucLuc.Controls.Add(this.button1);
+            this.panelMucLuc.Controls.Add(this.btnXemDSSach);
+            this.panelMucLuc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelMucLuc.Location = new System.Drawing.Point(-1, 2);
+            this.panelMucLuc.Name = "panelMucLuc";
+            this.panelMucLuc.Size = new System.Drawing.Size(213, 595);
+            this.panelMucLuc.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(-1, 431);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 93);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Danh sách nhân viên";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(-1, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 93);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "     Danh sách sách      bị hư";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(-1, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 93);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "     Danh sách sách      đang mượn";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(-1, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 93);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "     Danh sách sách      bị hư";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnXemDSSach
+            // 
+            this.btnXemDSSach.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnXemDSSach.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXemDSSach.Location = new System.Drawing.Point(-1, 59);
+            this.btnXemDSSach.Name = "btnXemDSSach";
+            this.btnXemDSSach.Size = new System.Drawing.Size(214, 93);
+            this.btnXemDSSach.TabIndex = 0;
+            this.btnXemDSSach.Text = "Danh sách các sách";
+            this.btnXemDSSach.UseVisualStyleBackColor = false;
+            this.btnXemDSSach.Click += new System.EventHandler(this.btnXemDSSach_Click);
+            // 
+            // FTrangChuToSach_ToTruong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 597);
+            this.Controls.Add(this.panelNen);
+            this.Name = "FTrangChuToSach_ToTruong";
+            this.Text = "FTrangChuToSach_ToTruong";
+            this.panelNen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxNen)).EndInit();
+            this.panelMucLuc.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelNen;
+        private System.Windows.Forms.Panel panelMucLuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnXemDSSach;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox picBoxNen;
+    }
+}
---
+++ Quan_Li_Thu_Vien/FTrangChuToSach_ToTruong.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Li_Thu_Vien
+{
+    public partial class FTrangChuToSach_ToTruong : Form
+    {
+        public FTrangChuToSach_ToTruong()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXemDSSach_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
