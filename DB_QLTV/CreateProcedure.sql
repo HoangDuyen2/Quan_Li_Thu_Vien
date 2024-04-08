50b6a50965ebd90e99a18291720414772c896395
DinhLoi2104 <157440186+DinhLoi2104@users.noreply.github.com>
2024-04-08 11:31:27 +0700

    thêm procedure sửa nhân viên

--- DB_QLTV/CreateProcedure.sql
+++ DB_QLTV/CreateProcedure.sql
@@ -31,6 +31,40 @@ BEGIN
 END
 GO
 --Kết thúc Procedure thêm nhân viên mới
+	
+--Bắt đầu sửa nhân viên mới
+CREATE PROCEDURE UpdateStaff (@TenNV nvarchar(32),
+								@GioiTinh nvarchar(1),
+								@NgaySinh DATETIME,
+								@DiaChi nvarchar(255),
+								@SDT varchar(10),
+								@Email varchar(50),
+								@MaNQL nvarchar(255),
+								@MaTT varchar(20),
+								@MaTo nvarchar(10))
+AS
+BEGIN
+	BEGIN TRANSACTION Tran_UpdateStaff
+	BEGIN TRY
+		UPDATE dbo.NhanVien SET TenNV = @TenNV,
+					GioiTinh = @GioiTinh,
+					NgaySinh = @NgaySinh,
+					DiaChi = @DiaChi,
+					SDT = @SDT,
+					Email = @Email,
+					MaNQL = @MaNQL,
+					MaTT = @MaTT,
+					MaTo = @MaTo
+		WHERE MaNV = @MaNV
+		COMMIT TRANSACTION Tran_UpdateStaff
+	END TRY
+	BEGIN CATCH
+		PRINT('Sửa không thành công')
+		ROLLBACK TRANSACTION Tran_UpdateStaff
+	END CATCH
+END
+GO
+--Kết Thúc Procedure sửa nhân viên
 
 --Bắt đầu xem nhân viên theo mã tổ của người tổ trưởng
 CREATE PROCEDURE [dbo].[proc_NhanVienTheoTo]
