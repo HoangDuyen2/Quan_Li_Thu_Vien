a10ff8af5bb01080dfc3f7bb05c31ab59a835c46
HoangDuyen2 <148479791+HoangDuyen2@users.noreply.github.com>
2024-04-08 09:42:07 +0700

    Update ThucThi.sql

--- DB_QLTV/ThucThi.sql
+++ DB_QLTV/ThucThi.sql
@@ -1,1 +1,3 @@
-
+--Bắt đầu thực thi lọc Nhân viên theo tổ người tổ trưởng đang đăng nhập
+exec proc_NhanVienTheoTo @MaTo = 'To01'
+--Kết thúc thực thi lọc Nhân viên theo tổ người tổ trưởng đang đăng nhập
