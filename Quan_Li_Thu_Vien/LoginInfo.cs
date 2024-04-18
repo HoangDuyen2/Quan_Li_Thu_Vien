1838f78c895e8b3de667f274cb174fc0c3a05b85
cocdeptraihaha <firefrog2804@gmail.com>
2024-04-19 05:20:31 +0700

    them thuoc tinh cho login info

--- Quan_Li_Thu_Vien/LoginInfo.cs
+++ Quan_Li_Thu_Vien/LoginInfo.cs
@@ -10,5 +10,7 @@ namespace Quan_Li_Thu_Vien
     internal class LoginInfo
     {
         public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string MaNV { get; set; }
     }
 }
