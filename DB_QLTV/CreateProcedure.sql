fd6934a4200a335ac51edbe3d4d848f02935f6ea
HoangDuyen2 <148479791+HoangDuyen2@users.noreply.github.com>
2024-04-24 22:16:12 +0700

    Update CreateProcedure.sql

--- DB_QLTV/CreateProcedure.sql
+++ DB_QLTV/CreateProcedure.sql
@@ -539,16 +539,6 @@ END
 GO
 --Kết Thúc Procedure sửa nhân viên
 
-
-
-
-
-
-
-
-
-
-
 --bắt đầu thêm độc giả mới
 CREATE PROCEDURE InsertDocGia
     @TenDocGia NVARCHAR(50),
@@ -558,18 +548,24 @@ CREATE PROCEDURE InsertDocGia
     @MaLoaiDG NVARCHAR(10)
 AS
 BEGIN
-    SET NOCOUNT ON;
+    BEGIN TRANSACTION Tran_InsertDocGia
+	BEGIN TRY
+		DECLARE @NgayTao DATE = CAST(GETDATE() AS DATE);
+		DECLARE @MaDocGia NVARCHAR(10) = [dbo].func_Auto_DocGiaID();
 
-    DECLARE @NgayTao DATE = CAST(GETDATE() AS DATE);
-    DECLARE @MaDocGia NVARCHAR(10) = [dbo].func_Auto_DocGiaID();
+		-- Thêm độc giả mới
+		INSERT INTO DocGia (MaDocGia, TenDocGia, Email, SoDienThoai, GioiTinh, MaLoaiDG, NgayTao)
+		VALUES (@MaDocGia, @TenDocGia, @Email, @SoDienThoai, @GioiTinh, @MaLoaiDG, @NgayTao);
 
-    -- Thêm độc giả mới
-    INSERT INTO DocGia (MaDocGia, TenDocGia, Email, SoDienThoai, GioiTinh, MaLoaiDG, NgayTao)
-    VALUES (@MaDocGia, @TenDocGia, @Email, @SoDienThoai, @GioiTinh, @MaLoaiDG, @NgayTao);
-
-    SELECT @MaDocGia AS MaDocGia;
+		COMMIT TRANSACTION Tran_InsertDocGia
+    END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION Tran_InsertDocGia
+		PRINT('Thêm không thành công!')
+	END CATCH
 END
 --kết thúc thêm đọc giả
+
 
 --bắt đầu sửa độc giả mới
 CREATE PROCEDURE UpdateDocGia
